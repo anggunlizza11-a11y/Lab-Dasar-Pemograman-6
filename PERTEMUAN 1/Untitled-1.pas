b0f0dcc3486015ff07c7de87bfcc484cd79b19e2
anggunlizza11-a11y <anggunlizza11@gmail.com>
2025-09-18 15:34:56 +0700

    pertemuan 1:pengenalan pascal

---
+++ PERTEMUAN 1/Untitled-1.pas
@@ -0,0 +1,6 @@
+uses crt;
+
+begin
+    clrscr;
+    writeln('hello world');
+end.
---
+++ PERTEMUAN 1/pengenalan.pas
@@ -0,0 +1,40 @@
+//program test;
+
+//unit library untuk menjalankan fungsi clear screen
+uses crt;
+
+var
+    //string
+    //nama_variabel : tipe variabel
+    nama : string;
+
+    //integer
+    umur : integer;
+
+    //char = huruf
+    jenis_kelamin : char;
+
+    //real = desimal
+    ip : real;
+
+//konstanta
+const
+   pi = 3.14;
+   gravitasi = 9.8;
+
+begin
+    clrscr;
+    //writeln('hello world');
+
+    writeln('nama mahasiswa : '); readln(nama);
+    writeln('umur mahasiswa : '); readln(umur);
+    writeln('jenis kelamin mahasiswa : '); readln(jenis_kelamin);
+    writeln('IP mahasiswa : '); readln(ip);
+
+    clrscr;
+    writeln('ini data yang anda masukkan');
+    writeln('nama mahasiswa yang anda input : ', nama);
+    writeln('umur mahasiswa yang anda input : ', umur);
+    writeln('jenis kelamin mahasiswa yang anda input :', jenis_kelamin);
+    writeln('IP mahasiswa yang anda input : ', ip:0:2);
+end.
